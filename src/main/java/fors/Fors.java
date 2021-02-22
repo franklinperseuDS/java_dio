ca1d4b00e1574f4f8bfea157fe69ba5fa510653a
franklinperseuDS <fpdll.cid20@uea.edu.br>
2021-02-21 19:24:51 -0800

    for simples

--- src/main/java/fors/Fors.java
+++ src/main/java/fors/Fors.java
@@ -1,4 +1,11 @@
 package fors;
 
 public class Fors {
+
+    public static void main(String[] args){
+
+        for (int i = 0; i <= 10; i++){
+            System.out.println("contador:"+i);
+        }
+    }
 }
