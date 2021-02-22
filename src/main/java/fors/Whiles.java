66157486fac1daf331cd0d8b8ea7cbccec24fcc5
franklinperseuDS <fpdll.cid20@uea.edu.br>
2021-02-21 19:30:29 -0800

    conceito de while e do..while

--- src/main/java/fors/Whiles.java
+++ src/main/java/fors/Whiles.java
@@ -1,4 +1,17 @@
 package fors;
 
 public class Whiles {
+    public static void main(String[] args){
+        var x= 0;
+        while (x < 1){
+            System.out.println("Dentro do while ....");
+            x++;
+        }
+
+        var y = 0;
+        do{
+            System.out.println("Dentro do do..while..");
+        }while(y++ <1);
+    }
 }
+
